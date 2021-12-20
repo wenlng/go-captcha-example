e72aca3d77bd08a5c93c2f600310cd8f756ff892
wenlng <wengaolng@gmail.com>
2021-12-20 20:28:11 +0800

    Updated version of go-captcha

--- main.go
+++ main.go
@@ -18,17 +18,22 @@ import (
 
 func main() {
 	// Example: Get captcha data
-	http.HandleFunc("/go_captcha_data", getCaptchaData)
+	http.HandleFunc("/api/go_captcha_data", getCaptchaData)
 	// Example: Post check data
-	http.HandleFunc("/go_captcha_check_data", checkCaptcha)
-	// Example: demo
-	http.HandleFunc("/go_captcha_demo", demo)
+	http.HandleFunc("/api/go_captcha_check_data", checkCaptcha)
+	// Example: demo、JS原生实现
+	http.HandleFunc("/go_captcha_example", demo)
+
+	// 这是静态资源----Vue版本
+	static := getPWD() + "/static/vue/"
+	fsh := http.FileServer(http.Dir(static))
+	http.Handle("/go_captcha_demo/", http.StripPrefix("/go_captcha_demo/",fsh))
 
 	// 临时定时清空缓存，由于是demo即在程序内部实现
 	runTimedTask()
 
-	log.Println("ListenAndServe 0.0.0.0:8001")
-	err := http.ListenAndServe(":8001", nil)
+	log.Println("ListenAndServe 0.0.0.0:9001")
+	err := http.ListenAndServe(":9001", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe err: ", err)
 	}
